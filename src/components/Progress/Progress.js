ba6b56ab8b312a3bd97cd352e347b1ca2d648d02
Mike Wheaton <mike@wheaton.design>
2019-06-16 13:44:39 -0700

    Switch to CSS variables for colors and rem (based on vw) for sizes

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -4,7 +4,13 @@ import "./Progress.css";
 const Progress = ({ progress }) => {
   return (
     <div className="Progress">
-      <div className="Progress-fill" style={{ width: `${progress * 100}%` }} />
+      <div
+        className="Progress-fill"
+        style={{
+          width: `${progress * 100}%`,
+          filter: `grayscale(${100 - progress * 100}%)`
+        }}
+      />
     </div>
   );
 };
